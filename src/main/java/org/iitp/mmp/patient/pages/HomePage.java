60fffd4961c25718520ec0a675796e703ec863e9
Seema Sinha <seema.sinha5@gmail.com>
2020-03-30 17:31:43 -0400

    home page

--- src/main/java/org/iitp/mmp/patient/pages/HomePage.java
+++ src/main/java/org/iitp/mmp/patient/pages/HomePage.java
@@ -1,6 +1,13 @@
 package org.iitp.mmp.patient.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
-	
+	 private  final WebDriver driver;
+	 public HomePage(WebDriver driver)
+		{
+			this.driver = driver;
+
+	     }
 
 }

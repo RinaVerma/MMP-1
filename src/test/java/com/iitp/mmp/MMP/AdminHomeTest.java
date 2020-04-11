1b9037769c3e87080f567ce95688e77d1e325510
sudheer <sudheer694@gmail.com>
2020-04-11 05:54:41 +0530

    sudheer cchanges

--- src/test/java/com/iitp/mmp/MMP/AdminHomeTest.java
+++ src/test/java/com/iitp/mmp/MMP/AdminHomeTest.java
@@ -10,7 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-      
+/**
+Sudheer changes
+**/
 public class AdminHomeTest {
 	@Test
 	public void adminAddReportsTest() throws InterruptedException, AWTException {
